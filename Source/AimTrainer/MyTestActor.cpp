f2d52624cc67361ac07a49161892b84a92402b8e
LunaFrrr <84046255+LunaFrrr@users.noreply.github.com>
2024-02-13 20:01:49 +0100

    Main menu widget & Tests

---
+++ Source/AimTrainer/MyTestActor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyTestActor.h"
+
+// Sets default values
+AMyTestActor::AMyTestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ValueA = 100;
+	ValueB = 20;
+}
+
+int32 AMyTestActor::CalculateValue() {
+	OnValueCalculate();
+	return ValueA + ValueB;
+}
+
+void OnValueCalculate_Implementation() {
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Blue, TEXT("Hello"));
+}
+
+// Called when the game starts or when spawned
+void AMyTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	CalculateValue();
+}
+
+// Called every frame
+void AMyTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
